a2ed5057713153efb5252a2763ae940db0442edc
perismuniu <peris.muniu@gmail.com>
2023-08-08 09:15:18 -0700

    print_comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for tens_digit in range(0, 10):
     for ones_digit in range(tens_digit + 1, 10):
-        print(f"{tens_digit:01d}{ones_digit:01d}", end="")
+        print("{:01d}{:01d}".format(tens_digit, ones_digit), end="")
         if tens_digit < 8 or ones_digit < 9:
             print(", ", end="")
         else:
