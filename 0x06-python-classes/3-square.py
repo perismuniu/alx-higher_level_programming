2838abbc06750cd0c42fa162e0c863154b7fab00
perismuniu <peris.muniu@gmail.com>
2023-08-29 11:19:20 -0700

    class Square that defines a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square instance"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+        def area(self):
+            """compute and return the area of the square"""
+            return self.__size**2
