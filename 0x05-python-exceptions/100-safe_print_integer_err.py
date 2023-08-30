e7fa62cee1c3eeb73eaaec1b7ce2a34f5024166d
perismuniu <peris.muniu@gmail.com>
2023-08-30 03:23:09 -0700

    function that prints an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def safe_print_integer_err(value):
-    import sys
 
+import sys
+
+def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
