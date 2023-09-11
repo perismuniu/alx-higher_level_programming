db115c06cf1d86fc0aee9269920478ed722d34b0
perismuniu <peris.muniu@gmail.com>
2023-09-11 13:02:16 -0700

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    This class defines a BaseGeometry.
+    """
+    def area(self):
+        """
+        Public instance method that raises an Exception with the message
+        'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method that validates the value.
+
+        Args:
+            name (str): The name of the value.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    This class defines a Rectangle, inheriting from BaseGeometry.
+    """
+    def __init__(self, width, height):
+        """
+        Initialize a Rectangle instance with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than or equal to 0.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
