3dd25b091f0f13779267c561660d08f797b3204f
perismuniu <peris.muniu@gmail.com>
2023-10-18 11:04:55 -0700

    11. Genre ID for all shows

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa.
 USE hbtn_0d_usa;
-SELECT cities.name
+SELECT id, name
 FROM cities
-WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY cities.id ASC;
+WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
+ORDER BY id;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Lists all cities contained in the database hbtn_0d_usa.
 USE hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
-FROM cities
-LEFT JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT c.id, c.name, s.name
+FROM cities as c
+INNER JOIN states AS s ON c.state_id = s.id
+ORDER BY c.id;
