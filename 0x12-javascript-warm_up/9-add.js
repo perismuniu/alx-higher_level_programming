aaf476c430dc6d647c29ad8359031f88cbf10f4b
perismuniu <peris.muniu@gmail.com>
2023-11-13 14:24:25 -0800

    10. Factorial

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 function add(a, b) {
-  return a + b;
-}
+  const c = a + b;
+  console.log(c);
+} 
 
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-
-console.log(add(num1, num2));
+add(Number(process.argv[2]), Number(process.argv[3]));
