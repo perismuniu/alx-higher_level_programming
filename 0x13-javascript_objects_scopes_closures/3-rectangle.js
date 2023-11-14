f70682934263813bdb904c5cccc4a101c79f9f94
perismuniu <peris.muniu@gmail.com>
2023-11-14 09:43:40 -0800

    3. Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,7 +6,17 @@ class Rectangle {
     }
     this.width = w;
     this.height = h; 
-  } 
+  }
+  
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
 }
 
 module.exports = Rectangle;
