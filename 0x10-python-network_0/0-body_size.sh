a1b5a4e450179b93c2d80643cf5bb32a56189ec1
perismuniu <peris.muniu@gmail.com>
2023-11-30 09:01:22 -0800

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+#Script that displays the Content-Length from a HTTP request
 
-#!/bin/bash
-
-URL=$1
-
-RESPONSE=$(curl -s $URL)
-SIZE=$(echo -n "$RESPONSE" | wc -c)
-
-echo $SIZE
+curl -s "$1" | wc -c
