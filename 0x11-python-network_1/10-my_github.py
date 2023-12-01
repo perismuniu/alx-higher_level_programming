c65d20e87133d491963f3aefb653b887683ece0d
perismuniu <peris.muniu@gmail.com>
2023-12-01 08:15:36 -0800

    9. My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A Python script that uses Basic Authentication with GitHubi
+API to display the user id.
+"""
+
+from requests import get, auth
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
