7603a746c56bef27755f5052a474d1e327712970
perismuniu <peris.muniu@gmail.com>
2024-01-03 07:09:54 -0800

    5. Loripsum - correction

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,4 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
-      if (writeError) {
-        console.error(writeError);
-      } else {
-        console.log(`Body response written to ${filePath}`);
-      }
-    });
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
