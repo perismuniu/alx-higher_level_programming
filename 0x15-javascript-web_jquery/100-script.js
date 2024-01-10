98938e438c82d6dc4892299ebee5d0575bfe5eeb
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-01-10 10:05:31 +0000

    10. No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+// 100-script.js
+document.addEventListener('DOMContentLoaded', function() {
+    var header = document.querySelector('header');
+    if (header) {
+      header.style.color = '#FF0000';
+    }
+  });
