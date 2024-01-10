070a82e8818d8a14e8eb5c7be379bbc53f4925e5
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-01-10 10:00:18 +0000

    9. Say Hello!

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $.ajax({
+      url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+      method: 'GET',
+      success: function(data) {
+        $('#hello').text(data.hello);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error('Error fetching translation:', errorThrown);
+      }
+    });
+  });
+  
